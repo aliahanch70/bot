59c7aea917c09b1bcbcffa6988c2f20da82b5f5b
aliahanch70 <86826859+aliahanch70@users.noreply.github.com>
2025-05-18 14:31:21 +0330

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $bot_token = '7458527169:AAHclRKmcrcAD4OSNEJBCM1kP4WvjfXmtCQ'
 $api_url = "https://api.telegram.org/bot$bot_token/";
 
-
+$my_id = 140867059;
 
 $input = file_get_contents("php://input");
-file_put_contents("log.txt", $input . PHP_EOL, FILE_APPEND);
+file_get_contents($api_url . "sendMessage?chat_id=$my_id&text=" . urlencode($input));
